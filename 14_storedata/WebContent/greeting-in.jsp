da7f1cc98fa0b4cb072c251f649bb96adbd42ff2
岩渕 誠也 <180453@jc-21.jp>
2020-11-26 12:59:55 +0900

    update

---
+++ 14_storedata/WebContent/greeting-in.jsp
@@ -0,0 +1,82 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="bean.BookBean" %>
+<%@ page import="javax.servlet.http.HttpSession" %>
+<%
+BookBean bookInfo = (BookBean)session.getAttribute("bookInfo");
+if(bookInfo == null) bookInfo = new BookBean();
+String errors = (String)session.getAttribute("errors");
+String[] checkbox_checked = {"","","","",""};
+if(bookInfo.getGenreList() != null){
+	for(String s : bookInfo.getGenreList().toArray(new String[bookInfo.getGenreList().size()])){
+		checkbox_checked[Integer.parseInt(s)] = "checked";
+	}
+}
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>書籍登録画面</title>
+</head>
+<body>
+	<h2>■書籍在庫管理システム</h2>
+	<h2><%=errors==null?"":errors %></h2>
+	<h3>【書籍登録画面】</h3>
+	<form action="<%=request.getContextPath()%>/getresult" method="post">
+		<table>
+			<tr>
+				<td></td>
+				<td>タイトル</td>
+				<td><input type="text" name="title"  value="<%=bookInfo.getTitle()==null?"":bookInfo.getTitle()%>"/></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>著者</td>
+				<td><input type="text" name="writer"   value="<%=bookInfo.getWritter()==null?"":bookInfo.getWritter() %>"/></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>出版社</td>
+				<td><input type="text" name="publisher"   value="<%=bookInfo.getPublisher()==null?"":bookInfo.getPublisher()%>"/></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>価格</td>
+				<td><input type="text" name="price"   value="<%=bookInfo.getPrice()==null?"":bookInfo.getPrice()%>"/></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>ジャンル</td>
+				<td><input type="checkbox" name="genre" value="0" <%=checkbox_checked[0] %>/>文芸 
+				<input	type="checkbox" name="genre" value="1"  		<%=checkbox_checked[1] %>/>実用
+				<input type="checkbox" name="genre"  value="2"  		<%=checkbox_checked[2] %> />ビジネス 
+				<input type="checkbox" name="genre" 	value="3"   	<%=checkbox_checked[3] %>/>教養 
+				<input type="checkbox" name="genre" value="4"   		<%=checkbox_checked[4] %>/>趣味
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>在庫</td>
+				<td><input type="radio" name="stock" value="1" <%=bookInfo.isStock()?"checked":"" %> />あり <input
+					type="radio" name="stock" value="0"  <%=!bookInfo.isStock()?"checked":"" %>/>なし</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>備考</td>
+				<td><textarea name="remarks" cols="40" rows="4"><%=bookInfo.getRemarks()==null?"":bookInfo.getRemarks()%></textarea></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td height="30"></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td align="right"><input type="submit" value="登録" /></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ 14_storedata/src/AddPageLogicServlet2.java
@@ -0,0 +1,76 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/getresult")
+public class AddPageLogicServlet2 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		List<String> params = new ArrayList<String>();
+		//セッション取得
+		HttpSession sessoin = request.getSession();
+		//validate
+		validate.Validator validator = new validate.Validator();
+		for(Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
+			params.add(e.nextElement());
+		}
+		boolean validateResult = validator.requiereCheck(params); //パラメータ数チェック
+		for(String paramName : params) validateResult = validator.requiereCheck(paramName, request.getParameterValues(paramName)) && validateResult;
+		//データ格納
+		bean.BookBean bookInfo = new bean.BookBean();
+		bookInfo.setTitle(request.getParameter("title"));
+		bookInfo.setWritter(request.getParameter("writer"));
+		bookInfo.setPublisher(request.getParameter("publisher"));
+		bookInfo.setRemarks(request.getParameter("remarks"));
+		try {
+			bookInfo.setPrice(request.getParameter("price"));
+		}catch(NumberFormatException e){
+			
+		}
+		//ジャンル処理
+		boolean[] genre_checked = {false,false,false,false,false};
+		if(request.getParameterValues("genre") != null) {
+			for (String s : request.getParameterValues("genre")) {
+				genre_checked[Integer.parseInt(s)] = true;
+			}
+			bookInfo.setGenreList(Arrays.asList(request.getParameterValues("genre")));
+		}
+		bookInfo.setStock(request.getParameter("stock").equals("0")?false:true);
+		bookInfo.setValidateResult(validateResult);
+		if(validateResult) {
+			//フォワード
+			//正常値のとき
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/greeting-out.jsp");//画面遷移準備
+			sessoin.setAttribute("bookInfo", bookInfo);//入力情報を次の画面に引き継ぐ
+			validator.clearErrors(); //エラーをクリア
+			sessoin.setAttribute("errors", validator.getErrors().toString());//エラー表示も引き継ぐ
+			dispatcher.forward(request, response);
+		}else {
+			//out.println(validator.getErrors());
+			//異常値の時
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/greeting-in.jsp");//画面遷移準備 入力画面に入力されたデータとともにエラーも送る。
+			sessoin.setAttribute("bookInfo", bookInfo);//入力情報を次の画面に引き継ぐ
+			sessoin.setAttribute("errors", validator.getErrors().toString());//エラー表示も引き継ぐ
+			dispatcher.forward(request, response);
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doGet(request,response);
+	}
+}
